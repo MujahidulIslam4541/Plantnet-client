33c9bd423d7fa390bbcf6372aed69fb5c99fb599
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-02-03 15:25:15 +0600

    user inpormation save to db

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -56,6 +56,14 @@ const AuthProvider = ({ children }) => {
       if (currentUser?.email) {
         setUser(currentUser)
 
+            await axios.post(`${import.meta.env.VITE_lOCALHOST_URL}/users/${currentUser?.email}`,
+              {
+                name:currentUser?.displayName,
+                image:currentUser?.photoURL,
+                email:currentUser?.email,
+
+              }
+            )
         // // Get JWT token
         // await axios.post(
         //   `${import.meta.env.VITE_API_URL}/jwt`,

c327c3d2c1b1ad2632691497af3a22c527ddb3af
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-02-13 19:00:57 +0600

    plant data added

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,34 @@
 import { Helmet } from 'react-helmet-async'
 import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { imageUpload } from '../../../api/utils'
+import useAuth from '../../../hooks/useAuth'
 
 const AddPlant = () => {
+  const { user } = useAuth()
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const from = e.target;
+    const name = from.name.value;
+    const description = from.description.value;
+    const category = from.category.value;
+    const price = parseFloat(from.price.value);
+    const quantity = parseInt(from.quantity.value);
+    const image = from.image.files[0];
+    const imageUrl = await imageUpload(image);
+
+    // Seller information
+    const seller = {
+      email: user?.email,
+      name: user?.displayName,
+      photo: user?.photoURL,
+    }
+
+    const plantData = {
+      name, description, category, price, quantity, image: imageUrl, seller
+
+    }
+    console.table(plantData)
+  }
   return (
     <div>
       <Helmet>
@@ -9,7 +36,7 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm handleSubmit={handleSubmit} />
     </div>
   )
 }

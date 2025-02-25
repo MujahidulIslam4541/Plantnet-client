211134446b7333665ba975e8ab93065b5e2c17a5
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-02-25 21:28:14 +0600

    dynamic routig

--- src/pages/PlantDetails/PlantDetails.jsx
+++ src/pages/PlantDetails/PlantDetails.jsx
@@ -6,22 +6,28 @@ import PurchaseModal from '../../components/Modal/PurchaseModal'
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import LoadingSpinner from '../../components/Shared/LoadingSpinner'
 
 const PlantDetails = () => {
+  const { id } = useParams()
   let [isOpen, setIsOpen] = useState(false)
-
+  console.log(id)
   const closeModal = () => {
     setIsOpen(false)
   }
 
-  const {data:plantsDetails=[]}=useQuery({
-    queryKey:['plantsDetails'],
-    queryFn: async()=>{
-      const { data } = await axios(`${import.meta.env.VITE_lOCALHOST_URL}/plants`)
+  const { data: plantsDetails = [], isLoading, refetch } = useQuery({
+    queryKey: ['plantsDetails', id],
+    queryFn: async () => {
+      const { data } = await axios(`${import.meta.env.VITE_lOCALHOST_URL}/plants/${id}`)
       return data
     }
   })
-
+  if (isLoading) {
+    return <LoadingSpinner></LoadingSpinner>
+  }
+  const {name,image,description,category,quantity,seller,price}=plantsDetails
   return (
     <Container>
       <Helmet>
@@ -34,7 +40,7 @@ const PlantDetails = () => {
             <div className='w-full overflow-hidden rounded-xl'>
               <img
                 className='object-cover w-full'
-                src='https://i.ibb.co/DDnw6j9/1738597899-golden-money-plant.jpg'
+                src={image}
                 alt='header image'
               />
             </div>
@@ -43,17 +49,15 @@ const PlantDetails = () => {
         <div className='md:gap-10 flex-1'>
           {/* Plant Info */}
           <Heading
-            title={'Money Plant'}
-            subtitle={`Category: ${'Succulent'}`}
+            title={name}
+            subtitle={`Category: ${category}`}
           />
           <hr className='my-6' />
           <div
             className='
           text-lg font-light text-neutral-500'
           >
-            Professionally deliver sticky testing procedures for next-generation
-            portals. Objectively communicate just in time infrastructures
-            before.
+           {description}
           </div>
           <hr className='my-6' />
 
@@ -67,7 +71,7 @@ const PlantDetails = () => {
                 gap-2
               '
           >
-            <div>Seller: Shakil Ahmed Atik</div>
+            <div>Seller: {seller?.name}</div>
 
             <img
               className='rounded-full'
@@ -75,7 +79,7 @@ const PlantDetails = () => {
               width='30'
               alt='Avatar'
               referrerPolicy='no-referrer'
-              src='https://lh3.googleusercontent.com/a/ACg8ocKUMU3XIX-JSUB80Gj_bYIWfYudpibgdwZE1xqmAGxHASgdvCZZ=s96-c'
+              src={seller?.photo}
             />
           </div>
           <hr className='my-6' />
@@ -87,12 +91,12 @@ const PlantDetails = () => {
                 text-neutral-500
               '
             >
-              Quantity: 10 Units Left Only!
+              Quantity: {quantity}Units Left Only!
             </p>
           </div>
           <hr className='my-6' />
           <div className='flex justify-between'>
-            <p className='font-bold text-3xl text-gray-500'>Price: 10$</p>
+            <p className='font-bold text-3xl text-gray-500'>Price: {price}$</p>
             <div>
               <Button label='Purchase' />
             </div>
